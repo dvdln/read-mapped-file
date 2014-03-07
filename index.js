62600efb10877fb75eec7d88be2b84ccf02863e2
David Lane <me@dvdln.com>
2014-03-07 14:24:38 -0500

    Update JSDocs

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-/*jshint node:true*/
-
 /**
  * @module read-mapped-file
+ *
+ * Read a file and its associated source map.
+ *
+ * @author David Lane <me@dvdln.com>
+ * @see {@link https://github.com/dvdln/read-mapped-file}
+ * @license MIT
  */
 module.exports = require('./lib/read-mapped-file');
+
+/**
+ * @callback ReadFileCallback
+ * @param {?Array.<string>} err
+ * @param {Source} data
+ */
+
+/**
+ * @typedef {Object} FileConfig
+ * @property {?string} src
+ * @property {?string} cwd
+ * @property {?string} mapSrc
+ * @property {?string} mapDir
+ * @property {?(Object.<string, string>|string)} fs
+ * @property {?string} code
+ * @property {?string} map
+ */
+
+/**
+ * @typedef {Object} Source
+ * @property {string} code
+ * @property {string} map
+ * @property {string} src
+ * @property {string} cwd
+ * @property {string} mapSrc
+ */
